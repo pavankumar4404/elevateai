8dde58aa38287331073e8382c8b6408cb691b7a8
Kalyanam Pavan Kumar <pavan4404kpk@gmail.com>
2025-07-01 00:38:57 +0530

    Routing

---
+++ actions/user.js
@@ -0,0 +1,102 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function updateUser(data) {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if(!user) {
+    throw new Error("User not found");
+  }
+  try{
+    const result = await db.$transaction(
+      async(tx) => {
+        //find if industry exists
+        let industryInsight = await tx.industryInsight.findUnique({
+          where:{
+            industry: data.industry,
+          },
+        });
+
+        //if not, create it with default values and replace with ai later
+        if(!industryInsight) {
+          industryInsight = await tx.industryInsight.create({
+            data: {
+              industry: data.industry,
+              salaryRanges: [], // Default empty array
+              growthRate: 0, // Default value
+              demandLevel: "Medium", // Default value
+              topSkills: [], // Default empty array
+              marketOutlook: "Neutral", // Default value
+              keyTrends: [], // Default empty array
+              recommendedSkills: [], // Default empty array
+              nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
+            },
+          });
+        }
+
+        //update user with new data
+        const updatedUser = await tx.user.update({
+          where: { id: user.id },
+          data: {
+            industry: data.industry,
+            experience: data.experience,
+            bio: data.bio,
+            skills: data.skills, // Link to the industry insight
+          },
+        });
+
+        return { updatedUser, industryInsight };
+      },
+      {
+        timeout: 10000,
+      }
+    );
+
+    return result.user;
+  }
+  catch(error) {
+    console.error("Error updating user:", error);
+    throw new Error("Failed to update profile");
+  }
+}
+
+export async function getUserOnboardingStatus() {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthenticated user");
+  }
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  try{
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+      select: {
+        industry: true,
+      },
+    })
+
+    return {
+      isOnboarded: !!user?.industry,
+    };
+  } 
+  catch (error) {
+    console.error("Error fetching user onboarding status:", error.message);
+    throw new Error("Failed to fetch onboarding status");
+  }
+}
---
+++ app/(main)/ai-cover-letter/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CoverLetter = async ({ params }) => {
+  const {id} = await params;
+
+  return <div>CoverLetter: {id}</div>;
+};
+
+export default CoverLetter;
